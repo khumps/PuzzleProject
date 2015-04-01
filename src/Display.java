29092cdfc48118a548d96ddaa797a49163d697f3
khumps <kmanh999@gmail.com>
2015-04-01 11:42:51 -0400

    Listener and Display update

--- src/Display.java
+++ src/Display.java
@@ -1,8 +1,14 @@
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 
 
 public class Display extends JFrame {
+private JComboBox menu;
 
+Display()
+{
+	
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- src/Listener.java
+++ src/Listener.java
@@ -1,4 +1,83 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 
 public class Listener {
+	private String command;
 
+	public class ButtonListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			command = e.getActionCommand();
+			if (command == "solve") {
+
+			}
+
+			if (command == "clear") {
+
+			}
+
+			if (command == "hint") {
+
+			}
+		}
+	}
+
+	public class KeyListener implements java.awt.event.KeyListener {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+	public class MouseListener extends MouseAdapter {
+		public void mouseClicked(MouseEvent e) {
+
+		}
+
+		public void mousePressed(MouseEvent e) {
+
+		}
+
+		public void mouseReleased(MouseEvent e) {
+
+		}
+
+		public void mouseEntered(MouseEvent e) {
+
+		}
+
+		public void mouseExited(MouseEvent e) {
+
+		}
+
+		public void mouseWheelMoved(MouseWheelEvent e) {
+
+		}
+
+		public void mouseDragged(MouseEvent e) {
+
+		}
+
+		public void mouseMoved(MouseEvent e) {
+
+		}
+
+	}
 }
