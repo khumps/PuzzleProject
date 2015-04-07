7318320bdba2863014237952a8ba6686d534126d
vijayjeeva1 <vijayjeeva1@gmail.com>
2015-04-07 11:33:40 -0400

    Commit

--- src/PuzzlePanel.java
+++ src/PuzzlePanel.java
@@ -4,32 +4,29 @@ import java.awt.Rectangle;
 import javax.swing.JPanel;
 
 public class PuzzlePanel extends JPanel {
+	
 	private PieceComponent[] p;	
 	private Puzzle pu;
-
 	
 	public PuzzlePanel(PieceComponent[] p){
-		this.p = p;
+		this.p = p;	
 	}
 	
 	public void paintComponent(Graphics g){
 		Graphics2D gr2 = (Graphics2D)g;
 		super.paintComponent(g);
-		
-		
 	}
 	
 	public void setPiece(int row, int col){
-		
+		Puzzle.setPiece(row,col);
 	}
 	
 	public void removePiece(int row, int col){
-		
+		Puzzle.setPiece(row,col);
 	}
 
 	
 	public static void main(String[] args) {
-
 
 	}
 
