f33f25f53863609290d7f79736319955c93e86a2
khumps <kmanh999@gmail.com>
2015-04-08 11:42:21 -0400

    good stuff

--- src/Listener.java
+++ src/Listener.java
@@ -32,7 +32,7 @@ public class Listener extends MouseAdapter implements ActionListener, KeyListene
 		}
 
 		if (command == "clear") {
-
+			display.getPuzzlePanel().getPuzzle().reset();
 		}
 
 		if (command == "hint") {
