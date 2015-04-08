d7877689fdc42c34de66f59ff022d479aa6c7d78
vijayjeeva1 <vijayjeeva1@gmail.com>
2015-04-08 11:36:02 -0400

    fill Rectangle

--- src/PuzzlePanel.java
+++ src/PuzzlePanel.java
@@ -3,6 +3,7 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.util.ArrayList;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class PuzzlePanel extends JPanel {
@@ -12,17 +13,25 @@ public class PuzzlePanel extends JPanel {
 	private Puzzle puzzle;
 	private JPanel unusedPiecePanel;
 	
+	public PuzzlePanel()
+	{
+		
+	}
 	public JPanel getUnusedPiecePanel() {
 		return unusedPiecePanel;
 	}
-
+	
 	public ArrayList<PieceComponent> getUnusedPieces() {
 		return unusedPieces;
 	}
-
+	
+	public void repaint(){
+		
+	}
 	public PuzzlePanel(Puzzle puzzle, Listener listener){
 		this.puzzle = puzzle;
 		pieces = new PieceComponent[puzzle.getUnusedPieces().size()];
+		
 		for(Piece p: puzzle.getUnusedPieces()){
 			
 		}
@@ -31,20 +40,28 @@ public class PuzzlePanel extends JPanel {
 	public void paintComponent(Graphics g){
 		Graphics2D gr2 = (Graphics2D)g;
 		super.paintComponent(g);
+		g.fillRect(0, 0, getWidth(), getHeight());
 	}
 	
 	public void setPiece(int row, int col, Piece p){
 		puzzle.setPiece(row, col, p);
+		
 	}
 	
 	public PieceComponent removePiece(int row, int col){
 		puzzle.removePiece(row,col);
 		return //PieceComponent that was there
 	}
+	
+	public void solve(){
+		Puzzle.solve();
+	}
 
 	
 	public static void main(String[] args) {
-
+		JFrame f = new JFrame();
+		f.add(new PuzzlePanel());
+		f.setVisible(true);
 	}
 
 }
