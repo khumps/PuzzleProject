a62d0b69e18e4589ac3ef9ac515d9f711c3c2e0f
khumps <kmanh999@gmail.com>
2015-04-11 20:09:50 -0400

    display overhaul

--- src/Display.java
+++ src/Display.java
@@ -1,3 +1,4 @@
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -9,6 +10,9 @@ import javax.swing.JTextField;
 import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Display extends JFrame {
 	private Listener listener;
@@ -16,10 +20,13 @@ public class Display extends JFrame {
 	private PuzzlePanel puzzlePanel;
 	private JPanel unusedPieces;
 	private Puzzle puzzle;
+	private Piece[] pieces = new Piece[9];
+	private BufferedImage[] imgs = new BufferedImage[9];
 	
 
 	Display() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		loadImages();
 		listener = new Listener(this);
 		menu = new JMenuBar();
 		JTextField instructions = new JTextField();
@@ -30,14 +37,40 @@ public class Display extends JFrame {
 		JMenuItem hint = Utils.newMenuItem(Utils.button,"Hint","clear",listener,menu);
 		JMenuItem solve = Utils.newMenuItem(Utils.button, "Solve", "solve", listener, menu);
 		setJMenuBar(menu);
-		puzzlePanel = new PuzzlePanel(puzzle,listener);
+		puzzlePanel = new PuzzlePanel(listener,imgs,pieces);
 		getContentPane().add(puzzlePanel);
 		pack();
 		setVisible(true);
 		
 
 	}
+	
+	private void loadImages()
+	{
+		for(int index = 0; index < 10; index++)
+		{
+			try{
+				imgs[index++] = ImageIO.read(new File("resources/piece_" + index + ".png"));
+			}
+			catch (IOException e) {
+				System.out.println("Hi");
+			}
+		}
+	}
 
+	private void createPieces()
+	{
+		pieces[0] = new Piece(Piece.CLUBS_OUT,Piece.HEARTS_OUT,Piece.DIAMONDS_IN,Piece.CLUBS_IN);
+		pieces[1] = new Piece(Piece.SPADES_OUT,Piece.DIAMONDS_OUT,Piece.SPADES_IN,Piece.HEARTS_IN);
+		pieces[2] = new Piece(Piece.HEARTS_OUT,Piece.SPADES_OUT,Piece.HEARTS_IN,Piece.CLUBS_IN);
+		pieces[3] = new Piece(Piece.HEARTS_OUT,Piece.DIAMONDS_OUT,Piece.CLUBS_IN,Piece.CLUBS_IN);
+		pieces[4] = new Piece(Piece.SPADES_OUT,Piece.SPADES_OUT,Piece.HEARTS_IN,Piece.CLUBS_IN);
+		pieces[5] = new Piece(Piece.HEARTS_OUT,Piece.DIAMONDS_OUT,Piece.DIAMONDS_IN,Piece.HEARTS_IN);
+		pieces[6] = new Piece(Piece.SPADES_OUT,Piece.DIAMONDS_OUT,Piece.HEARTS_IN,Piece.DIAMONDS_IN);
+		pieces[7] = new Piece(Piece.CLUBS_OUT,Piece.HEARTS_OUT,Piece.SPADES_IN,Piece.HEARTS_IN);
+		pieces[8] = new Piece(Piece.CLUBS_OUT,Piece.CLUBS_IN,Piece.DIAMONDS_IN,Piece.DIAMONDS_OUT);
+	}
+	
 	public PuzzlePanel getPuzzlePanel() {
 		return puzzlePanel;
 	}
