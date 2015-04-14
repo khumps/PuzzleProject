15847fc0a9e3f13ddeef63d5d6628722e8c4ca05
vijayjeeva1 <vijayjeeva1@gmail.com>
2015-04-14 16:29:16 -0400

    Commit

--- src/Grid.java
+++ src/Grid.java
@@ -15,9 +15,9 @@ import javax.swing.border.MatteBorder;
 
 public class Grid {
 
-    public static void main(String[] args) {
-        new Grid();
-    }
+//    public static void main(String[] args) {
+//        new Grid();
+//    }
 
     public Grid() {
         EventQueue.invokeLater(new Runnable() {
@@ -28,13 +28,6 @@ public class Grid {
                 } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
                 }
 
-                JFrame frame = new JFrame("Testing");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setLayout(new BorderLayout());
-                frame.add(new Pane());
-                frame.pack();
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
             }
         });
     }
