db3e8c1ad31d52bf92d12ab2167de831ff59e6e9
vijayjeeva1 <vijayjeeva1@gmail.com>
2015-04-14 11:12:27 -0400

    Comment

--- src/PuzzlePanel.java
+++ src/PuzzlePanel.java
@@ -95,21 +95,21 @@ public class PuzzlePanel extends JPanel {
 		JFrame f = new JFrame();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setSize(400, 500);
-		JPanel panel = new JPanel(new GridBagLayout());
-		f.getContentPane().add(panel, BorderLayout.NORTH);
-		
-		GridBagConstraints g = new GridBagConstraints();
-		g.insets = new Insets(10,10,10,10);
-		g.gridheight = 1;
-		g.gridwidth = 1;
+//	JPanel panel = new JPanel(new GridBagLayout());
+//	f.getContentPane().add(panel, BorderLayout.NORTH);
+//		
+//		GridBagConstraints g = new GridBagConstraints();
+//		g.insets = new Insets(10,10,10,10);
+//		g.gridheight = 1;
+//		g.gridwidth = 1;
 		PuzzlePanel p = new PuzzlePanel();
 		PieceComponent piece = new PieceComponent();
 		p.add(piece);
-		g.gridx = 0;
-		g.gridy = 0;
-		panel.add(piece, g);
+//		g.gridx = 0;
+//		g.gridy = 0;
+//		panel.add(piece, g);
 		f.setVisible(true);
-		f.add(panel);
+		//f.add(panel);
 		f.add(p);
 		f.pack();
 		
