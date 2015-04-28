334f532b4a9b6e84fd71203669df23343696b7d4
16karthika <16karthika@abschools.org>
2015-04-28 18:38:50 -0400

    Comments added.

--- src/Point.java
+++ src/Point.java
@@ -15,7 +15,7 @@ public class Point {
 	
 	// Prints the Point as a coordinate pair of the form (x, y)
 	
-	public String toString() {
+	public String toString(int x) {
 		return "(" + x + "," + y + ")";
 	}
 }
